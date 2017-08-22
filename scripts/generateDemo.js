9d18ab801a0f2a0b8df2abfd9486892be43316be
Wojciech Trocki <wtrocki@redhat.com>
2017-08-22 13:30:52 +0100

    Demo release tests

---
+++ scripts/generateDemo.js
@@ -0,0 +1,27 @@
+var ghpages = require('gh-pages');
+var path = require('path');
+
+// Debug tooling
+process.env.NODE_DEBUG = 'gh-pages';
+
+var version = '1.0.0'
+var module = 'demo-mobile-' + version;
+var options = {
+  branch: module,
+  dotfiles: true,
+  //Only add, and never remove existing files
+  add: true,
+  tag: 'release-' + module,
+  // Commit message
+  message: "Release " + module,
+  // Do not push (we will need to review and run tests before)
+  push: true
+}
+
+ghpages.publish("demo/mobile", options, function(err) {
+  if (err) {
+    console.info("Finished with error ", err);
+  } else {
+    console.info("Finished with success ");
+  }
+});
